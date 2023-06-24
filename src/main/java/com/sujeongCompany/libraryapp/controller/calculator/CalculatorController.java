642b84ef49d4491163414241137f57126390bae1
sujk912 <aa991122aa@gmail.com>
2023-06-24 15:33:55 +0900

    ADD Method with DTO

---
+++ src/main/java/com/sujeongCompany/libraryapp/controller/calculator/CalculatorController.java
@@ -0,0 +1,14 @@
+package com.sujeongCompany.libraryapp.controller.calculator;
+
+import com.sujeongCompany.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController  {
+
+    @GetMapping("/add") // GET /add
+    public int addTwoNumbers(CalculatorAddRequest request){
+        return request.getNumber1()+request.getNumber2();
+    }
+}
